d118bed8483a20d7c03c40168bef0bb58abb536e
sblendo00 <chri.sble@libero.it>
2019-10-22 11:43:07 +0000

    02.0

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'pug');   //Dico a express di usare pug come motore di template
+app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
 
 app.get('/', function (req, res) {
 //res.send('Hello World!');
