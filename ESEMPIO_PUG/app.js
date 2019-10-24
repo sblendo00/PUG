05af573ea539988cfd6e916d6dfd52d8c48850f9
sblendo00 <chri.sble@libero.it>
2019-10-24 11:57:01 +0000

    02.1

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const people = require('./people.json'); //Copia il file people.json dentro la variabile people
+const app = express();
+
 app.set('view engine', 'pug');   //Dico a express di usare pug come motore di template
 app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
 
 app.get('/', function (req, res) {
 //res.send('Hello World!');
 res.render('index', {
-   title: 'Homepage',
-   content : 'Questa pagina parla del mondo e di tanto altro'
+   title: 'HOMEPAGE',
+   people: people.profiles //Passa il vettore profiles alla pagina index.pug
  });
 });
 app.listen(3000, function () {
